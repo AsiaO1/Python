f8e390c22bd6bb0b673e419c3f98a2e4ebcb7987
Asia <ao@Asias-MacBook-Pro.local>
2023-03-18 20:59:46 +0900

    first commit

---
+++ hello.py
@@ -0,0 +1,10 @@
+# Ask user for their name
+name = input("What's your name? ")
+
+# Remove whitespace for str and capitalize user's name
+name = name.strip().title()
+
+# Split user's name into first name and last name
+first, last = name.split(" ")
+# Say hello to user
+print(f"hello, {first}")
