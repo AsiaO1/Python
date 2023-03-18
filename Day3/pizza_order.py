34022ca6c29caf9da01db572907b6375bd707b3b
Asia <ao@Asias-MacBook-Pro.local>
2023-03-18 23:21:58 +0900

    added Angela's solution

--- Day3/pizza_order.py
+++ Day3/pizza_order.py
@@ -22,3 +22,22 @@ if extra_cheese == "Y":
     bill += 1
 
 print(f"Your final bill is: ${bill}.")  
+#  Angela's variant:
+#
+# if size == "S":
+#     bill += 15
+# elif size == "M":
+#     bill += 20
+# else:
+#     bill += 25
+
+# if add_pepperoni == "Y":
+#     if size == "S":
+#         bill += 2
+#     else:
+#         bill += 3
+
+# if extra_cheese == "Y":
+#     bill += 1
+
+# print(f"Your final bill is ${bill}")
